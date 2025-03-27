3adbbcf31dafbce26e0833b1507faaa26e24a6ce
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-03-27 14:49:48 +0200

    Add the animations CSS file

---
+++ main.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 5; i++) {
+  let color;
+  if (i <= star) {
+    color = "text-yellow-200";
+  } else {
+    color = "text-gray-300";
+  }
+  `
+    <div> 
+     <i data-lucide='star' class='${color}'
+    </div>
+  `;
+}
