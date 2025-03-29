efb6f1b3470ce7c5592e9e34a466b20b6c14a542
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-03-29 17:29:29 +0200

    Disabled scrolling when the menu is opened

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const menuButton = document.getElementById("menu-button");
 // Toggle the 'hidden' class on the mobile nav when the button is clicked
 menuButton.addEventListener("click", () => {
   mobileNav.classList.toggle("hidden");
+  document.body.classList.toggle("overflow-hidden");
 });
 
 // Select the container element where hero cards will be added
