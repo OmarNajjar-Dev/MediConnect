769d790a345956773cbc49f6f4bd066e69e72252
elio142 <eliofaddoul244@gmail.com>
2025-04-03 22:05:47 +0300

    worked on the filter function for buttons

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ const container = document.getElementById('doctor-cards-container'); // Make sur
 
 doctors.map(doctor => {
   const doctorCard = `
-    <div id="doctor-card" class="backdrop-blur-md bg-card border border-solid border-card shadow-sm animate-fade-in rounded-lg p-6">
+    <div class="doctor-card backdrop-blur-md bg-card border border-solid border-card shadow-sm animate-fade-in rounded-lg p-6">
       <div class="flex flex-col h-full">
         <div class="flex items-start mb-4">
           <div class="w-20 h-20 rounded-full overflow-hidden mr-4 flex-shrink-0">
